47774582351874c33d0b85198ea3c3c780611f1d
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-17 10:46:50 -0700

    Update Simple-consecutive-pairs.js

--- Simple-consecutive-pairs.js
+++ Simple-consecutive-pairs.js
@@ -12,3 +12,12 @@ Your task will be to return the count of pairs that have consecutive numbers as 
 
 
 // Solution
+
+function pairs(array, count = 0) {
+  for (let i = 0; i < array.length; i += 2) {
+    if (Math.abs(array[i] - array[i + 1]) === 1) {
+      count += 1;
+    }
+  }
+  return count;
+}
